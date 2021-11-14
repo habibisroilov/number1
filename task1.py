5e7b8c60d9c2293ec2fad25380917e1f7f394c90
Habi Isroilov <habiisroilov345@gmail.com>
2021-11-14 17:29:49 +0500

    added some code

---
+++ task1.py
@@ -0,0 +1,6 @@
+num1 = int (input("Son kiriting "))
+num2 = int (input("Son kiriting "))
+num3 = int (input("Son kiriting "))
+num4 = int (input("Son kiriting "))
+for i in range (num1):
+    print(i)
